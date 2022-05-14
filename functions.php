2b1544e437773ee95339ce7885685076fa66bbed
Heneros <punk60826@gmail.com>
2022-05-14 21:18:05 +0300

    created function wood_load_more_script

--- functions.php
+++ functions.php
@@ -26,9 +26,9 @@ function add_menu_link_class( $atts, $item, $args ) {
 
 function wood_scripts(){
     wp_enqueue_script('main-script', get_template_directory_uri() . '/js/script.js', ['jquery'], true);
-    wp_enqueue_script('second-script',    get_template_directory_uri() . '/js/main.js',   ['jquery'], true);
+    // wp_enqueue_script('second-script',    get_template_directory_uri() . '/js/main.js',   ['jquery'], true);
 
-    wp_localize_script('second-script', 'wood', array('ajax_url' => admin_url('admin-ajax.php')));
+    // wp_localize_script('second-script', 'wood', array('ajax_url' => admin_url('admin-ajax.php')));
 
 
     wp_enqueue_style('main-style', get_template_directory_uri() . '/css/style.css', [], '1.0', 'all');
@@ -43,43 +43,40 @@ add_action("wp_ajax_nopriv_loadmore", "load_more_posts");
 
 
 function load_more_posts(){
-    $paged = !empty($_POST['paged']) ? $_POST['paged'] : 1;
-    $paged++;
+//     $paged = !empty($_POST['paged']) ? $_POST['paged'] : 1;
+//     $paged++;
 
-   $args = array(
-       'paged' => $paged,
-       'post_status' => 'publish'
-   );
+//    $args = array(
+//        'paged' => $paged,
+//        'post_status' => 'publish'
+//    );
 
-   $taxonomy = !empty($_POST['taxonomy']) ? $_POST['taxonomy'] : '';
-   $term_id  = !empty($_POST['term_id']) ? $_POST['term_id'] : '';
+//    $taxonomy = !empty($_POST['taxonomy']) ? $_POST['taxonomy'] : '';
+//    $term_id  = !empty($_POST['term_id']) ? $_POST['term_id'] : '';
 
-   if($taxonomy && $term_id){
-       $args['tax_query'] = array(
-           array(
-               'taxonomy' => $taxonomy,
-               'terms'    => $term_id
-           )
-           );
-   }
+//    if($taxonomy && $term_id){
+//        $args['tax_query'] = array(
+//            array(
+//                'taxonomy' => $taxonomy,
+//                'terms'    => $term_id
+//            )
+//            );
+//    }
 
-   query_posts($args);
-   ob_start();
-   $query = new WP_Query([
-    'post_type' => 'post',
-    'order' => 'ASC'
-]);
-if($query->have_posts()):
-while ($query->have_posts()):
-   $query->the_post();
-   get_template_part('theme-parts/content-article');
-   endwhile;
-endif;
+//    query_posts($args);
+//    ob_start();
+//    $query = new WP_Query([
+//     'post_type' => 'post',
+//     'order' => 'ASC'
+// ]);
+// if($query->have_posts()):
+// while ($query->have_posts()):
+//    $query->the_post();
+//    get_template_part('theme-parts/content-article');
+//    endwhile;
+// endif;
+//    die();
 
-
-
-
-   die();
 
 }
 
@@ -107,5 +104,20 @@ function wood_setup() {
     add_image_size( 'homepage-thumb', 360, 200 ); 
 }
     
+
+function wood_load_more_script(){
+    global $wp_query;
+    wp_enqueue_script('jquery');
+    wp_register_script( 'my_loadmore', get_stylesheet_directory_uri() . '/js/main.js', array('jquery') );
+
+    wp_localize_script('my_loadmore', 'wood_loadmore_params', array(
+        'ajaxurl' => site_url() . '/wp-admin/admin-ajax.php', 
+		'posts' => json_encode( $wp_query->query_vars ), 
+		'current_page' => get_query_var( 'paged' ) ? get_query_var('paged') : 1,
+		'max_page' => $wp_query->max_num_pages
+    ));
+    wp_enqueue_script('my_loadmore');
+}
+add_action("wp_enqueue_scripts", "wood_load_more_script");
 
 ?>
