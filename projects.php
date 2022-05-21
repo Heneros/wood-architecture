bbdfb70c6fb93e6c365162760adb15f6aadc8202
Heneros <punk60826@gmail.com>
2022-05-21 23:57:59 +0300

    display posts from our-projects

--- projects.php
+++ projects.php
@@ -19,9 +19,45 @@ get_header();
                     <?php the_excerpt(); ?>
                 </p>
             </div>
-            
         </div>
     </div>
+    <?php
+    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+    $query = new WP_Query([
+        'post_type' => 'our-projects',
+        'paged' => $paged,
+        'posts_per_page' => -1
+    ]);
+    if ($query->have_posts()) :
+    ?>
+        <div class="blocks__items">
+            <?php
+            while ($query->have_posts()) :
+                $query->the_post();
+                $featured_img = get_the_post_thumbnail_url()
+            ?>
+                <div class="item__image">
+                    <div class="hovereffect">
+                        <img src="<?= $featured_img; ?>" alt="">
+
+                        <div class="overlay">
+                            <div class="info">
+                                <span>Atlas </span>
+                                <h2><?php the_title(); ?></h2>
+                                <a href="<?= get_permalink(); ?>">Learn more</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php
+            endwhile;
+            ?>
+        </div>
+    <?php
+    endif;
+    ?>
+</div>
+</div>
 </div>
 <?php
 get_footer();
