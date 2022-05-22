d5c625b52f0bc5d1a64b14e038ca40767d67d9b7
Heneros <punk60826@gmail.com>
2022-05-23 00:14:10 +0300

    single-our-projects created

---
+++ single-our-projects.php
@@ -0,0 +1,75 @@
+<?php
+
+get_header();
+
+$gallery_images = get_field('gallery_images');
+$size = 'full';
+
+?>
+<section class="project">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="gallery">
+                    <div class="swiper mySwiperGallery2">
+                        <div class="swiper-wrapper">
+                            <?php
+                            if ($gallery_images) :
+                                foreach ($gallery_images as $gallery_image) : ?>
+                                    <div class="swiper-slide">
+                                        <img src="<?php echo esc_url($gallery_image['url']) ?>" alt="<?php echo esc_attr($gallery_image['alt']) ?>">
+                                    </div>
+                            <?php
+                                endforeach;
+                            endif;
+                            ?>
+                        </div>
+                    </div>
+                    <div class="swiper mySwiperGallery1">
+                        <div class="swiper-wrapper">
+                            <?php
+                            if ($gallery_images) :
+                                foreach ($gallery_images as $gallery_image) : ?>
+                                    <div class="swiper-slide">
+                                        <img src="<?php echo esc_url($gallery_image['url']) ?>" alt="<?php echo esc_attr($gallery_image['alt']) ?>">
+                                    </div>
+                            <?php
+                                endforeach;
+                            endif;
+                            ?>
+                        </div>
+                    </div>
+                </div>
+                <!--Gallery ends-->
+            </div>
+        </div>
+    </div>
+</section>
+<section class="description__project">
+    <div class="container">
+        <div class="row">
+            <h1 class="main_title__description-project">
+                <?php the_title(); ?>
+            </h1>
+            <div class="container__description">
+                <div class="col-md-8">
+                    <div class="project__content">
+                        <?php the_content(); ?>
+                    </div>
+
+                </div>
+                <div class="col-md-4">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+<?php
+
+get_footer();
+
+?>
